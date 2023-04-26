b950eeed1a5c485af674aeba0350474f3912e062
Ndiogou <ndiogoudiop01@gmail.com>
2023-04-26 17:44:58 +0000

    modified:   lib/pages/produit_details.dart 	modified:   pubspec.lock

--- bmb/lib/pages/produit_details.dart
+++ bmb/lib/pages/produit_details.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class ProduitDetails extends StatefulWidget {
@@ -11,7 +13,7 @@ class ProduitDetails extends StatefulWidget {
     this.product_detail_picture,
     this.product_detail_price,
     this.product_detail_old_price,
-});
+  });
 
   @override
   State<ProduitDetails> createState() => _ProduitDetailsState();
@@ -21,7 +23,7 @@ class _ProduitDetailsState extends State<ProduitDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  new AppBar(
+      appBar: new AppBar(
         elevation: 0.1,
         backgroundColor: Colors.red,
         title: Text('Boutique bmb'),
@@ -33,7 +35,7 @@ class _ProduitDetailsState extends State<ProduitDetails> {
               ),
               onPressed: () {}),
           new IconButton(
-            // ignore: prefer_const_constructors
+              // ignore: prefer_const_constructors
               icon: Icon(
                 Icons.shopping_cart,
                 color: Colors.white,
@@ -50,8 +52,210 @@ class _ProduitDetailsState extends State<ProduitDetails> {
                 color: Colors.white,
                 child: Image.asset(widget.product_detail_picture),
               ),
+              footer: new Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: new Text(
+                    widget.product_detail_nom,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  title: new Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text(
+                          "\CFA ${widget.product_detail_price}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.red),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
-          )
+          ),
+
+          //Premier button ========================
+          Row(
+            children: <Widget>[
+              //------ la taille du bouton -------------
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text('taille'),
+                            content: new Text('Choisir la taille'),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text('Fermer'),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text("Taille")),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              //------ la taille du bouton -------------
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text('Couleur'),
+                            content: new Text('Choisir la couleur'),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text('Fermer'),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text("Couleur")),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              //------ la taille du bouton -------------
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text('Quantite'),
+                            content: new Text('Choisir la quantite'),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text('Fermer'),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text("Quantite")),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+//       -------- Deuxieme Boutons -----------------------
+          //Premier button ========================
+          Row(
+            children: <Widget>[
+              //------ la taille du bouton -------------
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 0.2,
+                  child: new Text('Acheter'),
+                ),
+              ),
+              new IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add_shopping_cart),
+                color: Colors.red,
+              ),
+              new IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border),
+                color: Colors.red,
+              ),
+            ],
+          ),
+          Divider(),
+          new ListTile(
+            title: new Text("Detail Produit"),
+            subtitle: new Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry."),
+          ),
+          Divider(),
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "Nom Produit",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: new Text("${widget.product_detail_nom}"),
+              )
+            ],
+          ),
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "Type Produit",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: new Text("Categorie prod"),
+              )
+            ],
+          ),
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "Condition Produit",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: new Text("etat"),
+              )
+            ],
+          ),
         ],
       ),
     );
